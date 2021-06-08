10eeae35f0d973fb7b1c606dcc6f11cab9fd39a5
Balaji Shanmugavel <sbalaji@protonmail.com>
2021-06-08 16:37:00 +0530

    Added lifecycle events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    console.log('inside constructor')
+    super();
+    this.state = {
+      temperature: '',
+      value: 0,
+    }
+  }
+
+  componentDidMount() {
+    console.log('inside componentDidMount')
+    //Add event listener
+    //Api fetch
+    //Url params
+    fetch('https://goweather.herokuapp.com/weather/thoothukudi')
+    .then(res => res.json())
+    .then(res => {
+      // console.log('data', res);
+      this.setState({temperature: res.temperature})
+    })
+    window.addEventListener('resize', this.resize)
+  }
+
+  resize = () => {
+    console.log('window.innerWidth', window.innerWidth)
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log('inside componentDidUpdate', prevState, 'current', this.state)
+    if(prevState.temperature !== this.state.temperature) {
+      alert('State updated')
+      this.setState({value: 10})
+    }
+
+  }
+
+  componentWillUnmount () {
+    //Show alert to user
+    //Remove event listener
+    window.removeEventListener('resize', this.resize)
+  }
+
+  handleClick = () => {
+
+    
+  }
+
+
+
+  render(){
+    console.log('inside render')
+    return(<div>
+      <Temperature temperature={this.state.temperature}/>
+      <button onClick={this.handleClick}>Submit</button>
+      <button onClick={() => this.setState({value: 20})}>Update Value</button>
+      </div>)
+  }
+}
+
+class Temperature extends Component {
+  render(){
+    // console.log('state', this.state, 'props')
+    return <div>{this.props.temperature}</div>
+  }
 }
 
 export default App;
+
+
+//constructor
+//render
+//componentDidMount
+//render
+// componentDidUpdate
+// componentWillUnmount
